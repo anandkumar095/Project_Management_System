df3d16c2f1c82b656d3fbadcef5ac15d0aae3fa2
raghulchandran123 <104768658+raghulchandran123@users.noreply.github.com>
2024-11-10 12:46:33 +0530

    Frontend code for instructor

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,14 @@ import Login from '../components/loginPage.vue';  // Your Login component
 import Dashboard from '../components/StudentDashboard.vue'; // Your Dashboard component (optional)
 import myteam from '../components/myTeam.vue';
 
+import InsDash from "@/views/InsDash.vue";
+import ProjectGuidelines from "@/views/ProjectGuidelines.vue";
+
+import MilestoneGuidelines from "@/views/MilestoneGuidelines.vue";
+import ProjectList from "@/views/ProjectList.vue";
+import DocumentReview from "@/views/DocumentReview.vue";
+import AllStudents from "@/views/AllStudents.vue";
+
 // Telling Vue to use Vue Router
 Vue.use(VueRouter);
 
@@ -12,7 +20,38 @@ Vue.use(VueRouter);
 const routes = [
   { path: '/', component: Login },  // Login page at root
   { path: '/dashboard', component: Dashboard },
-  {path:'/dashboard/myteam',component:myteam}  // Dashboard route (if created)
+  {path:'/dashboard/myteam',component:myteam},  // Dashboard route (if created)
+  {
+    path: "/instructor/:id",
+    name: "instructordashboard",
+    component: InsDash,
+  },
+  {
+    path: "/projectguidelines/:id",
+    name: "projectguidelines",
+    component: ProjectGuidelines,
+  },
+
+  {
+    path: "/milestoneguidelines/:id",
+    name: "milestoneguidelines",
+    component: MilestoneGuidelines,
+  },
+  {
+    path: "/projectlist/:id",
+    name: "projectlist",
+    component: ProjectList,
+  },
+  {
+    path: "/documentreview/:id",
+    name: "documentreview",
+    component: DocumentReview,
+  },
+  {
+    path: "/allstudents/:id",
+    name: "allstudents",
+    component: AllStudents,
+  },
 ];
 
 // Create the router instance
